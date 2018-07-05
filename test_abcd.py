832896d0fc0384835692417255ddeed785ac7862
Jeffrey Salmond <js947@cam.ac.uk>
2018-07-05 14:56:29 +0100

    add test cases for ABCD class

---
+++ test_abcd.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from pytest import fixture
+
+@fixture
+def db():
+    import abcd
+    return abcd.ABCD('postgresql://localhost/abcd')
+           
+
+def test_count(db):
+    db.count()
+
+def test_select(db):
+    db.select('total_energy')
+
+def test_keys(db):
+    db.keys()
