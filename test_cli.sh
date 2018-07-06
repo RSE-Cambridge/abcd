65bbfbbda18f3f09e9a533982d542293fed51b91
Jeffrey Salmond <js947@cam.ac.uk>
2018-07-06 16:59:10 +0100

    refactor

--- test_cli.sh
+++ test_cli.sh
@@ -20,6 +20,8 @@ run_abcd stats total_energy
 run_abcd stats total_energy where $q
 run_abcd stats total_energy degauss
 run_abcd stats total_energy degauss where $q
-run_abcd count  where total_energy lt -269950
-run_abcd delete where total_energy lt -269950
-run_abcd count  where total_energy lt -269950
+
+q='total_energy lt -269950'
+run_abcd count  where $q
+run_abcd delete where $q
+run_abcd count  where $q
