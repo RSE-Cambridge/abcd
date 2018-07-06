d8410dfdd4d0133d892af8993b828f66c802f387
Jeffrey Salmond <js947@cam.ac.uk>
2018-07-06 18:18:19 +0100

    add some write tests

--- test_cli.sh
+++ test_cli.sh
@@ -21,7 +21,11 @@ run_abcd stats total_energy where $q
 run_abcd stats total_energy degauss
 run_abcd stats total_energy degauss where $q
 
+run_abcd write out.xyz
+run_abcd write out.xyz $q
+
 q='total_energy lt -269950'
 run_abcd count  where $q
 run_abcd delete where $q
 run_abcd count  where $q
+
