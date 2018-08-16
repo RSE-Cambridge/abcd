5bc218740c68d89c00a81032c9cbf9028e1f8afa
Jeffrey Salmond <js947@cam.ac.uk>
2018-08-16 17:49:01 +0100

    add setup.py script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+        name='abcd',
+        version='0.1',
+        packages=['abcd'],
+        scripts=['abcd.py'],
+        install_requires=['psycopg2', 'pandas', 'ply', 'ase']
+        )
