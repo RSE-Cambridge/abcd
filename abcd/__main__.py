e979f153fe10bbe9cf0ddadd5ca44786b52f6cf8
Jeffrey Salmond <js947@cam.ac.uk>
2018-08-16 23:05:15 +0100

    move abcd.py to __main__

---
+++ abcd/__main__.py
@@ -0,0 +1,20 @@
+from os import getenv
+from argparse import ArgumentParser
+from abcd import Shell
+
+def main():
+    parser = ArgumentParser(description='import xyz files to database')
+    parser.add_argument('--db', help="database connection string", default=getenv("ABCD_DB", 'postgresql://localhost'))
+    parser.add_argument('-v', help="verbose- print info about database interactions", action="store_true")
+    parser.add_argument('query', help="optional query string", nargs='*')
+
+    args = parser.parse_args()
+
+
+    if args.query:
+        Shell(args.db, args.v).onecmd(' '.join(args.query))
+    else:
+        Shell(args.db, args.v).cmdloop(intro='connected to %s' % args.db)
+
+if __name__ == '__main__':
+    main()
