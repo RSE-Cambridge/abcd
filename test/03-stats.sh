7c684c68588e5ce9cd19d44395de22a500620dcd
Jeffrey Salmond <js947@cam.ac.uk>
2018-08-19 22:43:31 +0100

    adjust failing test case to avoid floating point error

--- test/03-stats.sh
+++ test/03-stats.sh
@@ -28,22 +28,22 @@ max      -10.822042
 EOF
 }
 
-@test "stats total_energy degauss" {
-diff <(abcd stats total_energy degauss) - <<EOF
-            degauss   total_energy
+@test "stats total_energy ecutwfc" {
+diff <(abcd stats total_energy ecutwfc) - <<EOF
+            ecutwfc   total_energy
 count  1.414300e+04   59112.000000
-mean   1.360569e-01  -18677.444930
-std    2.775656e-17   60558.715973
-min    1.360569e-01 -449910.248128
-25%    1.360569e-01   -3460.893854
-50%    1.360569e-01    -132.677221
-75%    1.360569e-01    -126.756328
-max    1.360569e-01     -10.822042
+mean   1.224512e+03  -18677.444930
+std    2.273817e-13   60558.715973
+min    1.224512e+03 -449910.248128
+25%    1.224512e+03   -3460.893854
+50%    1.224512e+03    -132.677221
+75%    1.224512e+03    -126.756328
+max    1.224512e+03     -10.822042
 EOF
 }
 
-@test "stats total_energy degauss where total_energy gt -15" {
-diff <(abcd stats total_energy degauss where total_energy gt -15) - <<EOF
+@test "stats total_energy ecutwfc where total_energy gt -15" {
+diff <(abcd stats total_energy ecutwfc where total_energy gt -15) - <<EOF
        total_energy
 count  12000.000000
 mean     -11.099061
